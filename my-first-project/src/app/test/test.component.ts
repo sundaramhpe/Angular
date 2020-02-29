3ae1717ab924f4e4315b145da3af9d22a202e16d
sundaramhpe <sunram_c@hotmail.com>
2020-02-29 16:41:48 +0530

    Interpolation And Style binding Added

--- my-first-project/src/app/test/test.component.ts
+++ my-first-project/src/app/test/test.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  public name="Sundaram C";
+  public myId="TestId"
+  public successClass="text-success"
+  public hasError = true;
+  public isSpecial =true;
+  public messageClasses ={
+    "text-success": this.hasError,
+    "text-danger": this.hasError,
+    "text-sprcial":this.isSpecial
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  greetUser(){
+    return "Hello "+this.name;
   }
 
 }
