2e935b84856fdc7cd60cebf82f0ffbc67f9534a1
sundaramhpe <sunram_c@hotmail.com>
2020-02-29 16:57:21 +0530

    child component created

--- binding-perant-child-project/src/app/app.module.ts
+++ binding-perant-child-project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestChildComponent } from './test-child/test-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestChildComponent
   ],
   imports: [
     BrowserModule
---
+++ binding-perant-child-project/src/app/test-child/test-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-child',
+  templateUrl: './test-child.component.html',
+  styleUrls: ['./test-child.component.css']
+})
+export class TestChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
