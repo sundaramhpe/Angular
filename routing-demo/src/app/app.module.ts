46f20ea7a30257e2f4a0900ef6caf02108fc7e74
sundaramhpe <sunram_c@hotmail.com>
2020-03-01 15:44:39 +0530

    Router Configured

--- routing-demo/src/app/app.module.ts
+++ routing-demo/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
